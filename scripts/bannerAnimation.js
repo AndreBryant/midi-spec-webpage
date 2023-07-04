fc485656e055daa0a82daddefbb947dd8998866a
embryzon <95128949+embryzon@users.noreply.github.com>
2023-07-04 11:23:22 +0800

    .

--- scripts/bannerAnimation.js
+++ scripts/bannerAnimation.js
@@ -1,1 +1,8 @@
 // Here goes the banner animation (matrix animation sana)
+//matrix of zeros that turns the hovered digit into a 1.
+window.addEventListener('load', ()=>{
+    const canvas = document.getElementById('canvas1');
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    console.log(canvas.height,canvas.width);
+})
