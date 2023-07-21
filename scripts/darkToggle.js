4c58599183221f9ef55e93be9d2722cdf40df491
embryzon <95128949+embryzon@users.noreply.github.com>
2023-07-21 15:20:52 +0800

    .

---
+++ scripts/darkToggle.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', () => {
+    var darkToggle = document.querySelector('.toggler');
+    var all = document.querySelector('.the-forbidden')
+    var value = 0;
+    darkToggle.addEventListener('click', () => {
+        if(!value){
+            all.style.filter=`invert(${value++})`;
+        } else {
+            all.style.filter=`invert(${value--})`;
+        }
+        console.log(value)
+    })
+
+    // Add event listeners for mouse events
+    darkToggle.addEventListener('mousedown', () => {
+        darkToggle.classList.add('active'); // Apply a class when the button is pressed
+        setTimeout(()=>{darkToggle.classList.remove('active')}
+        ,1000)
+    });
+
+})
